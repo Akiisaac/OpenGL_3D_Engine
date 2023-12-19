c630274e836287faf71c9c89a5807a537bba0e76
Akiisaac <akiisaac32@gmail.com>
2023-12-19 18:54:13 +0100

    Initial setups

---
+++ Debug/CMakeFiles/OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL.dir/src/main.c.o"
+  "CMakeFiles/OpenGL.dir/src/main.c.o.d"
+  "OpenGL"
+  "OpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,13 @@
+// 3rd Party Libraries
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+
+int main(int arc, char* argv[])
+{
+  printf("th is going on");
+
+	return 0;
+}
+
